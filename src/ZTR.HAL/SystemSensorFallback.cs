82ec20e5c4687f555174f39180a5bdb825a2b935
ZTR_OS <ztr-os@example.com>
2026-08-26 08:50:41 +0000

    fix: CS1503 - use ManagementScope instead of string for WMI queries

--- src/ZTR.HAL/SystemSensorFallback.cs
+++ src/ZTR.HAL/SystemSensorFallback.cs
@@ -317,8 +317,9 @@ public class SystemSensorFallback : ISystemSensorFallback
 
         try
         {
+            var scope = new System.Management.ManagementScope(@"\\.\Root\WMI");
             using var searcher = new System.Management.ManagementObjectSearcher(
-                @"\\.\Root\WMI",
+                scope,
                 new System.Management.ObjectQuery("SELECT * FROM MSAcpi_ThermalZoneTemperature"));
             using var results = searcher.Get();
 
@@ -448,8 +449,9 @@ public class SystemSensorFallback : ISystemSensorFallback
     {
         try
         {
+            var scope = new System.Management.ManagementScope(@"\\.\Root\WMI");
             using var searcher = new System.Management.ManagementObjectSearcher(
-                @"\\.\Root\WMI",
+                scope,
                 new System.Management.ObjectQuery("SELECT * FROM MSAcpi_ThermalZoneTemperature"));
             using var results = searcher.Get();
 
